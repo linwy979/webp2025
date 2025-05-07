e966a0cac1d8ccda7a393791284340bb96a19ef0
lin979 <86038968+linwy979@users.noreply.github.com>
2025-05-07 14:58:50 +0800

    Add files via upload

---
+++ lab13/react_login/cgu_hello.jsx
@@ -0,0 +1,11 @@
+// src/cgu_hello.js
+const styleArgument = {
+    fontSize: '100px',
+    color: 'red'
+  };
+  
+  const HelloCGU = () => {
+    return <h1 style={styleArgument}>hello CGU!!</h1>;
+  };
+  
+  export default HelloCGU;
